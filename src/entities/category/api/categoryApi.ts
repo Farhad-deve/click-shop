b65c02d7e01b43ebd772f79b90a48bcfb36c935f
Farhad-deve <Farkhad0527@gmail.com>
2026-09-19 15:59:24 +0500

    Files

--- src/entities/category/api/categoryApi.ts
+++ src/entities/category/api/categoryApi.ts
@@ -1,11 +1,18 @@
 import { baseApi } from "../../../shared/api";
-import type { Category } from "../model/types";
+import type { Category, CategoryResponse } from "../model/types";
 
 export const categoryApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getCategories: builder.query<Category[], void>({
       query: () => "/categories/get",
       providesTags: ["Category"],
+    }),
+    addCategory: builder.mutation<CategoryResponse, FormData>({
+      query: (formData) => ({
+        url: '/categories/add',
+        method: 'POST',
+        body: formData,
+      })
     }),
     deleteCategory: builder.mutation<void, string>({
       query: (id) => ({
--- src/entities/category/model/types.ts
+++ src/entities/category/model/types.ts
@@ -4,3 +4,10 @@ export interface Category {
     description: string;
     image: string;
 }
+
+export interface CategoryResponse {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+}
